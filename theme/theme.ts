909e0ba8a091901498497d7360b4ab7f85f176a8
Savenok_Oleh <o.savenok@wezom.com>
2023-06-22 16:05:03 +0300

    feature - refactor

--- theme/theme.ts
+++ theme/theme.ts
@@ -1,10 +1,10 @@
-import {createTheme} from '@mui/material/styles'
+import { createTheme } from '@mui/material/styles'
 
-export const theme = createTheme({
+export let theme = createTheme({
     palette: {
         primary: {
-            main: '#ffd700'
-        }
+            main: '#ffd700',
+        },
     },
     breakpoints: {
         values: {
@@ -12,7 +12,20 @@ export const theme = createTheme({
             sm: 530,
             md: 768,
             lg: 1024,
-            xl: 2560
-        }
+            xl: 2560,
+        },
     },
 })
+
+theme = {
+    ...theme,
+    components: {
+        MuiTypography: {
+            styleOverrides: {
+                root: {
+                    fontFamily: '"Nunito", sans-serif',
+                }
+            }
+        }
+    },
+}
